a69929212b9ea81506e7809557b08bc503e5921d
Borroq <skrzypek.dsrw@gmail.com>
2024-04-07 23:28:37 +0200

    Virst commit
    Application sketch

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/example/SimplePaintApp.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class SimplePaintApp extends JFrame {
+    private Color currentColor = Color.BLACK; //Domyślny kolor lini
+    private int strokeSize = 5; //Domyślna grubość lini
+    private BufferedImage canvasImage; // Obraz płótna do rysowania
+    private Point linkStart = null; //Punkt początkowy lini, zainicjowany jako null
+
+    public SimplePaintApp()  {
+        super("Prosta aplikacja do rysowania");
+        canvasImage = new BufferedImage(800, 600, BufferedImage.TYPE_4BYTE_ABGR);
+        fillCanvas(Color.WHITE); //Wypełnienie płótna kolorem białym
+        DrawingPanel drawingPanel = new DrawingPanel;
+        getContentPane().add(drawingPanel, BorderLayout.CENTER);
+
+        createToolbar(); // Tworzenie paska narzędzi
+
+        setSize(800,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
